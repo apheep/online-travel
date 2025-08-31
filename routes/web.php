3ed8f300d4b622d453024d89cd2774cc2554d4ed
faiz <faizdaffanaut@gmail.com>
2025-08-31 22:58:01 +0700

    pesawar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/pesanan/pesawat', function () {
+    return view('pesanan.pesawat');
 });
